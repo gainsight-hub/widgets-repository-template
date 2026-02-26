a44950dee2c2c40b1c2ce3a466d04957a3221741
Łukasz Wnęk <lukasz.wnek@gainsight.com>
2026-02-26 11:15:14 +0100

    debug(greeting): test document.querySelector vs shadowRoot
    GE-241941

--- widgets/greeting/app.js
+++ widgets/greeting/app.js
@@ -1,16 +1,17 @@
 export async function init(sdk) {
   await sdk.whenReady();
   const render = (props) => {
-    const card = sdk.shadowRoot.querySelector('.card');
-    const titleEl = sdk.shadowRoot.querySelector('#title');
-    const messageEl = sdk.shadowRoot.querySelector('#message');
+    const card = sdk.shadowRoot.querySelector(".card");
+    const titleEl = sdk.shadowRoot.querySelector("#title");
+    const messageEl = sdk.shadowRoot.querySelector("#message");
     card.style.background = props.color;
     titleEl.textContent = props.title;
     messageEl.textContent = props.message;
+    console.log("elo", document.querySelector(".card"));
   };
   render(sdk.getProps());
-  sdk.on('propsChanged', render);
-  sdk.on('destroy', () => {
-    sdk.shadowRoot.querySelector('.card').innerHTML = '';
+  // sdk.on('propsChanged', render)
+  sdk.on("destroy", () => {
+    sdk.shadowRoot.querySelector(".card").innerHTML = "";
   });
 }

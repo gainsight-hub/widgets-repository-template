4f0c5e5211d9f3d7b69849760fa41fa72dd7db60
Łukasz Wnęk <lukasz.wnek@gainsight.com>
2026-03-09 17:58:46 +0100

    revert: remove absolute-script-urls plugin
    GE-242583

--- widgets/react_widget/vite.config.ts
+++ widgets/react_widget/vite.config.ts
@@ -1,24 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ mode }) => {
-  const { VITE_TUNNEL_URL } = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [
-      react(),
-      {
-        name: "absolute-script-urls",
-        transformIndexHtml: (html) =>
-          VITE_TUNNEL_URL
-            ? html.replace(/src="\/(.*?)"/g, `src="${VITE_TUNNEL_URL}/$1"`)
-            : html,
-      },
-    ],
-    server: {
-      allowedHosts: true,
-      cors: true,
-      hmr: false,
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: true,
+    cors: true,
+    hmr: false,
+  },
 });
